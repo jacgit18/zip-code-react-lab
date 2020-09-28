f7c49d3409fa446a1b8ca92f8b39a1e1e0e9089f
jac <joshuacarpentier21@gmail.com>
2020-09-28 17:21:02 -0400

    edits

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -7,20 +7,48 @@ function City(props) {
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  
+  return (
+  <div>
+    Type zip code
+    <input type="text" onChange={props.zipChanged} value={props.zipValue} />
+    <p>You entered: {props.zipValue}</p>
+  </div>
+  );
 }
 
 
 class App extends Component {
+  state = {
+    userInputValue: "",
+          // add property
+
+  }
+
+  handleZipChange(event){
+    console.log(event.target.value);
+    fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
+    .then(response => response.json())
+    .then(jsonData => {
+      this.setState({
+        //result
+      })
+    });
+    this.setState({
+      userInputValue: event.target.value
+    })
+  }
+
+  // look at boards project for render for city 
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.userInputValue} />
         <div>
-          <City />
+          <City /> 
           <City />
         </div>
       </div>
---
+++ test/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+
+// eslint-disable-next-line
+const welcome = { greeting: "Hey" };
+function getTitle(title) {
+  return title;
+}
+
+const list = [
+  {
+  title: 'React',
+  url: 'https://reactjs.org/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: 'Redux',
+  url: 'https://redux.js.org/',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+  },
+  ];
+
+
+function App() {
+  return (
+    <div>
+      <h1>
+        {welcome.greeting} {getTitle('react')}
+      </h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+export default App;

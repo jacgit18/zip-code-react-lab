9eb52e3046c65fc610b66a561d1d1d0aa9ee3f28
jac <joshuacarpentier21@gmail.com>
2020-10-01 12:51:55 -0400

    testing things

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+  <div>
+  This is the City component
+  <p>You entered: {props.results}  </p>
+
+  </div>);
 }
 
 function ZipSearchField(props) {
@@ -12,7 +17,7 @@ function ZipSearchField(props) {
   <div>
     Type zip code
     <input type="text" onChange={props.zipChanged} value={props.zipValue} />
-    <p>You entered: {props.zipValue}</p>
+    <p>You entered: {props.zipValue}  </p>
   </div>
   );
 }
@@ -21,8 +26,13 @@ function ZipSearchField(props) {
 class App extends Component {
   state = {
     userInputValue: "",
-          // add property
-
+    userResultArray: [],
+    userCity:"",
+    userState:"",
+    userLocation:"",
+    userEstimatedPopulation:"",
+    userTotalWages:"",
+    // intialize State
   }
 
   handleZipChange(event){
@@ -30,17 +40,34 @@ class App extends Component {
     fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
     .then(response => response.json())
     .then(jsonData => {
+      console.log(jsonData);
       this.setState({
-        //result
+         userResultArray: jsonData.results
+        //  userResultArray: [userCity.City, userState, userLocation, userEstimatedPopulation, userTotalWages]
+        // if (this.state.currSelected < this.props.numBoards - 1)
+        // this.setState({ currSelected: this.state.currSelected + 1 });
+        //   else this.setState({ currSelected: 0 });
+
+        // console.log(); for testing
+        // City State
+        //result  State Location EstimatedPopulation TotalWages
       })
     });
     this.setState({
       userInputValue: event.target.value
+      
+      // Update State
     })
   }
 
   // look at boards project for render for city 
   render() {
+    // let {userResultArray} = this.state.userCity;
+    let {userResultArray} = this.state;
+    for (let ii = 0; ii < this.props.userResultArray; ii++) {
+      let isSelected = ii === this.state.userResultArray; 
+      userResultArray.push(<App index={ii} selected={isSelected} key={ii} />);
+    }
     return (
       <div className="App">
         <div className="App-header">
@@ -48,8 +75,10 @@ class App extends Component {
         </div>
         <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.userInputValue} />
         <div>
-          <City /> 
-          <City />
+        <City zipChanged={(e) => this.handleZipChange(e)} cityValue={this.state.userResultArray}  /> 
+          {/* <City zipChanged={(e) => this.handleZipChange(e)} cityValue={this.state.userResultArray[0]}  stateValue={this.state.userResultArray} /> 
+          <City zipChanged={(e) => this.handleZipChange(e)} stateValue={this.state.userResultArray} 
+          Location={this.state.userResultArray} EstimatedPopulation={this.state.userResultArray} TotalWages={this.state.userResultArray}  /> */}
         </div>
       </div>
     );

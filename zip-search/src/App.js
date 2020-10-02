dcc9f1c669d3ee7933b6048b5fd841736e5ed84c
jac <joshuacarpentier21@gmail.com>
2020-10-01 21:37:34 -0400

    tried some stuff

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 
 function City(props) {
   return (
-  <div>
-  This is the City component
-  <p>You entered: {props.results}  </p>
-
-  </div>);
+<div>
+      <h2> {props.locationText} </h2>
+      <ul>
+        <li> State: {props.state} </li>
+        <li> Location: {props.location} </li>
+        <li> Population: {props.population} </li>
+        <li> Total Wages: {props.totalWages}</li>
+      </ul>
+    </div>
+    );
 }
 
 function ZipSearchField(props) {
@@ -26,23 +31,20 @@ function ZipSearchField(props) {
 class App extends Component {
   state = {
     userInputValue: "",
-    userResultArray: [],
-    userCity:"",
-    userState:"",
-    userLocation:"",
-    userEstimatedPopulation:"",
-    userTotalWages:"",
+    
     // intialize State
   }
 
+   url = 'http://ctp-zip-api.herokuapp.com/zip/';
   handleZipChange(event){
     console.log(event.target.value);
-    fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
+    if(event.target.value.length >= 5){
+    fetch(this.url+event.target.value)
     .then(response => response.json())
     .then(jsonData => {
       console.log(jsonData);
       this.setState({
-         userResultArray: jsonData.results
+         userResultArray: jsonData
         //  userResultArray: [userCity.City, userState, userLocation, userEstimatedPopulation, userTotalWages]
         // if (this.state.currSelected < this.props.numBoards - 1)
         // this.setState({ currSelected: this.state.currSelected + 1 });
@@ -51,8 +53,10 @@ class App extends Component {
         // console.log(); for testing
         // City State
         //result  State Location EstimatedPopulation TotalWages
-      })
+    })
+      
     });
+  }
     this.setState({
       userInputValue: event.target.value
       
@@ -60,14 +64,15 @@ class App extends Component {
     })
   }
 
+
   // look at boards project for render for city 
   render() {
-    // let {userResultArray} = this.state.userCity;
-    let {userResultArray} = this.state;
-    for (let ii = 0; ii < this.props.userResultArray; ii++) {
-      let isSelected = ii === this.state.userResultArray; 
-      userResultArray.push(<App index={ii} selected={isSelected} key={ii} />);
-    }
+    // // let {userResultArray} = this.state.userCity;
+    // let {userResultArray} = this.state;
+    // for (let ii = 0; ii < this.state.userResultArray; ii++) {
+    //   let isSelected = ii === this.state.userResultArray; 
+    //   userResultArray.push(<App index={ii} selected={isSelected} key={ii} />);
+    // }
     return (
       <div className="App">
         <div className="App-header">
@@ -79,6 +84,7 @@ class App extends Component {
           {/* <City zipChanged={(e) => this.handleZipChange(e)} cityValue={this.state.userResultArray[0]}  stateValue={this.state.userResultArray} /> 
           <City zipChanged={(e) => this.handleZipChange(e)} stateValue={this.state.userResultArray} 
           Location={this.state.userResultArray} EstimatedPopulation={this.state.userResultArray} TotalWages={this.state.userResultArray}  /> */}
+
         </div>
       </div>
     );

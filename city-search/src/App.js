f628bd97b18607fe2f4dd51fd381fd5f3b5ac883
jacgit18 <Joshua.Carpentier@mail.citytech.cuny.edu>
2020-10-07 17:45:53 -0400

    fix

--- city-search/src/App.js
+++ city-search/src/App.js
@@ -1,55 +1,91 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 function City(props) {
-  return (<div>This is the City component</div>);
-}
-
-function ZipSearchField(props) {
-  
   return (
-  <div>
-    Type zip code
-    <input type="text" onChange={props.zipChanged} value={props.zipValue} />
-    <p>You entered: {props.zipValue}</p>
-  </div>
+    <div>
+      <h2>
+        {props.data.City}, {props.data.State}
+      </h2>
+      <ul>
+        <li>State: {props.data.State}</li>
+        <li>Location: ({props.data.Lat}, {props.data.Long}) </li>
+        <li>Estimated Population: {props.data.EstimatedPopulation}</li>
+        <li>Total Wages: {props.data.TotalWages}</li>
+      </ul>
+    </div>
   );
 }
 
+function ZipSearchField(props) {
+  return (
+    <div>
+      Type zip code
+      <input type="text" onChange={props.zipChanged} value={props.zipValue} />
+      <p>You entered: {props.zipValue} </p>
+    </div>
+  );
+}
 
 class App extends Component {
   state = {
     userInputValue: "",
-          // add property
+    userResultArray: []
+    // intialize State
+  };
 
-  }
 
-  handleZipChange(event){
-    console.log(event.target.value);
-    fetch('http://ctp-zip-api.herokuapp.com/zip/'+event.target.value)
-    .then(response => response.json())
-    .then(jsonData => {
-      this.setState({
-        //result
-      })
-    });
+
+  url = "http://ctp-zip-api.herokuapp.com/zip/";
+  handleZipChange(event) {
+
     this.setState({
       userInputValue: event.target.value
+                          // Update State
+    
     })
-  }
+  
+  console.log(event.target.value);
+  if (event.target.value.length === 5) {
+    fetch(this.url + event.target.value)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log(jsonData);
+        this.setState({
+          userResultArray: jsonData,
+                    // Update State
 
-  // look at boards project for render for city 
+        });
+      }).catch(err => this.setState({ userResultArray: [] }));
+     
+  } else {
+    this.setState({
+      userResultArray: [],
+
+    })
+
+  }
+}
+
   render() {
+ 
+    
+    console.log(this.state.userResultArray);
+
+    
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.userInputValue} />
+        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.userInputValue}/>
         <div>
-          <City /> 
-          <City />
+          {this.state.userResultArray.map((item) => {
+            return <City data={item} />;
+          })}
+
+      
+      
         </div>
       </div>
     );
--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -5,8 +5,17 @@ function City(props) {
   return (
     <div>
       <h2>
-        {this.data.City}, {this.data.State}
+        {props.data.City}, {props.data.State}
       </h2>
+      <ul>
+        <li>State: {props.data.State}</li>
+        <li>Location: ({props.data.Lat}, {props.data.Long}) </li>
+        <li>Estimated Population: {props.data.EstimatedPopulation}</li>
+        <li>Total Wages: {props.data.TotalWages}</li>
+
+
+
+      </ul>
     </div>
   );
 }
@@ -24,42 +33,49 @@ function ZipSearchField(props) {
 class App extends Component {
   state = {
     userInputValue: "",
-
+    userResultArray: []
     // intialize State
   };
 
+
+
   url = "http://ctp-zip-api.herokuapp.com/zip/";
   handleZipChange(event) {
-    console.log(event.target.value);
-    if (event.target.value.length >= 5) {
-      fetch(this.url + event.target.value)
-        .then((response) => response.json())
-        .then((jsonData) => {
-          console.log(jsonData);
-          this.setState({
-            userResultArray: jsonData,
-            //  userResultArray: [userCity.City, userState, userLocation, userEstimatedPopulation, userTotalWages]
-            // if (this.state.currSelected < this.props.numBoards - 1)
-            // this.setState({ currSelected: this.state.currSelected + 1 });
-            //   else this.setState({ currSelected: 0 });
 
-            // console.log(); for testing
-            // City State
-            //result  State Location EstimatedPopulation TotalWages
-          });
-        });
-    }
     this.setState({
-      userInputValue: event.target.value,
+      userInputValue: event.target.value
+                          // Update State
+    
+    })
+  
+  console.log(event.target.value);
+  if (event.target.value.length === 5) {
+    fetch(this.url + event.target.value)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        console.log(jsonData);
+        this.setState({
+          userResultArray: jsonData,
+                    // Update State
 
-      // Update State
-    });
+        });
+      }).catch(err => this.setState({ userResultArray: [] }));
+     
+  } else {
+    this.setState({
+      userResultArray: [],
+
+    })
+
   }
+}
 
   // look at boards project for render for city
   render() {
-    // can see latest state and not calling state better for testing
-    console.log(this.state.results);
+    // can see latest state and not calling state better for testing 
+    // when in the handler we were trying to run before the fetch ran 
+    
+    console.log(this.state.userResultArray);
 
     // // let {userResultArray} = this.state.userCity;
     // let {userResultArray} = this.state;
@@ -72,17 +88,20 @@ class App extends Component {
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField
-          zipChanged={(e) => this.handleZipChange(e)}
-          zipValue={this.state.userInputValue}
-        />
+        <ZipSearchField zipChanged={(e) => this.handleZipChange(e)} zipValue={this.state.userInputValue}/>
         <div>
-          {this.state.results.map((item) => {
-            return <City data={} />;
+          {this.state.userResultArray.map((item) => {
+            return <City data={item} />;
           })}
-          {/* <City zipChanged={(e) => this.handleZipChange(e)} cityValue={this.state.userResultArray[0]}  stateValue={this.state.userResultArray} /> 
-          <City zipChanged={(e) => this.handleZipChange(e)} stateValue={this.state.userResultArray} 
-          Location={this.state.userResultArray} EstimatedPopulation={this.state.userResultArray} TotalWages={this.state.userResultArray}  /> */}
+
+           {/*
+           not a hash map this is a built in map function 
+           this function takes an array and apply a funtion to each element in
+           the array making a new array with the transformed array map and and foreach but
+           foreach wont return new array like map which transform data like in this case
+           the json array into a new array that is being returned
+            */}
+      
         </div>
       </div>
     );
